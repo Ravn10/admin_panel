c6723f7ca195f7ea7c0eaa780031bf5c520f0e2e
Aniket <anikets06121989@gmail.com>
2021-02-09 12:30:31 +0000

    changes in hook

--- admin_panel/hooks.py
+++ admin_panel/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/admin_panel/css/admin_panel.css"
-app_include_js = "https://cdn.jsdelivr.net/npm/frappe-charts@1.2.4/dist/frappe-charts.min.iife.js"
+# app_include_js = "https://cdn.jsdelivr.net/npm/frappe-charts@1.2.4/dist/frappe-charts.min.iife.js"
 
 # include js, css files in header of web template
 # web_include_css = "assets/css/frappe_charts.min.css"
