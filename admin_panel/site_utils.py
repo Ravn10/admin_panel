f8633cd43a34d0ae72925db7bae5002c25a3a732
Aniket Shinde <anikets@criscoconsulting.in>
2021-03-08 08:11:28 +0000

    some changes

--- admin_panel/site_utils.py
+++ admin_panel/site_utils.py
@@ -13,13 +13,6 @@ def create_site_for_saas(site_name, install_erpnext, key, domain):
     mysql_password = get_decrypted_password("SAAS Settings","SAAS Settings","db_password")
     admin_password = get_decrypted_password("SAAS Settings","SAAS Settings","admin_password")
     custom_domain = domain + "."+ frappe.db.get_single_value("SAAS Settings","saas_domain")
-    # site_domain_map = frappe.get_doc({
-    #                             "doctype":"Site Domain Map",
-    #                             "site":site_name,
-    #                             "domain":custom_domain
-    #                         })
-    # site_domain_map.insert()
-    frappe.db.commit()
     create_site(site_name, install_erpnext, mysql_password, admin_password, key)
 
 def update_custom_domain(doc, method=None):
@@ -34,5 +27,14 @@ def update_custom_domain(doc, method=None):
 
 def send_email_on_site_creation(doc,method=None):
     if doc.status == "Success":
-        pass
-    pass
+        frappe.publish_progress(
+			99  / 100,
+			title = "Site Created",
+			description = "Site Created"
+		)
+    else:
+        frappe.publish_progress(
+			99  / 100,
+			title = "Site Not Created",
+			description = "Site Not Created"
+		)
--- admin_panel/www/prepare_site/index.py
+++ admin_panel/www/prepare_site/index.py
@@ -2,8 +2,9 @@ no_cache = 1
 
 import frappe
 from frappe.core.doctype.user.user import get_timezones
-from erpnext.shopping_cart.cart import get_party
+from erpnext.shopping_cart.cart import get_party, get_cart_quotation
 from admin_panel.site_utils import create_site_for_saas
+from bench_manager.bench_manager.utils import verify_whitelisted_call, safe_decode
 
 def get_context(context):
     context.company_domain = frappe.db.get_single_value("SAAS Settings","saas_domain")
@@ -18,10 +19,30 @@ def get_context(context):
     if frappe.form_dict.subdomain:
         context.title = frappe.form_dict.subdomain
         customer_doc =  get_party()
-        customer_doc.site = frappe.form_dict.subdomain
         customer_doc.customer_detail = str(frappe.form_dict)
         create_site_for_saas(site_name=frappe.form_dict.subdomain,install_erpnext=True,
                     key=format(frappe.utils.datetime.date.today()),
                     domain=frappe.form_dict.subdomain)
         context.thanks = "block"
         context.form_3 = "none"
+    
+@frappe.whitelist()
+def get_party_():
+    return get_party()
+
+@frappe.whitelist(allow_guest=True)
+def create_site(site_name, install_erpnext, mysql_password, admin_password, key):
+	verify_whitelisted_call()
+	commands = ["bench new-site --mariadb-root-password {mysql_password} --admin-password {admin_password} {site_name}".format(site_name=site_name,
+		admin_password=admin_password, mysql_password=mysql_password)]
+	if install_erpnext == "true":
+		with open('apps.txt', 'r') as f:
+			app_list = f.read()
+		if 'erpnext' not in app_list:
+			commands.append("bench get-app erpnext")
+		commands.append("bench --site {site_name} install-app erpnext".format(site_name=site_name))
+	frappe.enqueue('bench_manager.bench_manager.utils.run_command',
+		commands=commands,
+		doctype="Bench Settings",
+		key=key
+	)
